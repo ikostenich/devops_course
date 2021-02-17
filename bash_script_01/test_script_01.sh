62f93205a2a22bba1b8845b918ef74d21290665e
ikostenich <i.kostenich@gmail.com>
2021-02-17 17:15:09 +0000

    removed unnecessary files

--- bash_script_01/test_script_01.sh
+++
@@ -1,5 +0,0 @@
-sudo find /home -type d -mtime +1 -printf "\n%AD %AT %h %f" | sort -k4 | awk -v red=$(tput setaf 1) -v white=$(tput setaf 7) '{
-        $1=red $1
-        $2=red $2
-        $3=white $3
-     } 1'
